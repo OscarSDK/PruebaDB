38ea34d44b0a82b0d5386f94dddb2238390b52bd
[Luis <[luheligo72@gmail.com]>
2021-11-15 10:52:03 -0500

    Otro Cambio

--- src/dbproyectogit/DBProyectoGIT.java
+++ src/dbproyectogit/DBProyectoGIT.java
@@ -6,6 +6,10 @@ public class DBProyectoGIT {
     public static void main(String[] args) {
        String nombre = "Oscar";
        String Apellido = "Condori";
+       
+       String nombre2 = "Luis";
+       String Apellido2 = "Linares";
+       
     }
     
 }
